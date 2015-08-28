9225e1a09adc4eccb2d5b09855242ee41c6436ea
David Chanin <dchanin@quizlet.com>
2015-08-27 23:48:17 -0400

    using a list comprehension instead

--- Problem10.hs
+++ Problem10.hs
@@ -2,7 +2,5 @@ module Problem10 ( lengthEncode ) where
 
 import Data.List
 
-encode groupedVal@(val:_) accum = (length groupedVal, val) : accum
-
 lengthEncode :: (Eq a) => [a] -> [(Int, a)]
-lengthEncode list = foldr encode [] $ group list
+lengthEncode list = [(length x, head x) | x <- group list]
